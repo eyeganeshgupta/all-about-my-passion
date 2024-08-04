4faaa2fb11883321f271626cc7fff667f03ea494
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-08-04 08:16:37 +0530

    project-setup🛠️

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="font-semibold">Project Setup</h1>
+    </div>
+  );
+}
+
+export default App;
